65d47fc422fe4a170d839eaf6a436db88f847c3d
Linnchoeuh <lennintendo@gmail.com>
2024-07-02 19:20:57 +0200

    feat: performance improvement
    No longer loading models at each api call
    No longer saving -> loading -> erasing image to hardrive

--- main.py
+++ main.py
@@ -13,13 +13,34 @@ from src.middlewares.init import InitMiddleware
 from src.endpoints.ping import ping
 from src.endpoints.get_alphabet import get_alpahabet
 
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe.tasks.python.vision.hand_landmarker import *
+
+from src.alphabet_recognizer import *
+
+def load_hand_tracker_model() -> HandLandmarker:
+    base_options = python.BaseOptions(model_asset_path="hand_landmarker.task")
+    options: HandLandmarkerOptions = vision.HandLandmarkerOptions(base_options=base_options,
+                                                                  num_hands=1,
+                                                                  min_hand_detection_confidence=0,
+                                                                  min_hand_presence_confidence=0.1,
+                                                                  min_tracking_confidence=0)
+    recognizer: HandLandmarker = vision.HandLandmarker.create_from_options(options)
+    return recognizer
+
+def load_alphabet_recognizer_model() -> LSFAlphabetRecognizer:
+    model: LSFAlphabetRecognizer = LSFAlphabetRecognizer()
+    model.loadModel("model.pth")
+    return model
+
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument(
         '--port',
         help='Port the Flask app will listen on.',
         required=False,
-        default=8080)
+        default=5000)
     parser.add_argument(
         '--debug',
         help='To run Flask app in debug mode.',
@@ -32,6 +53,11 @@ def main():
     # Setup logger
     logger: logging.Logger = setup_logger(args.debug)
     logger.debug(f"Logger setup")
+
+    hand_tracker: HandLandmarker = load_hand_tracker_model()
+    alphabet_recognizer: LSFAlphabetRecognizer = load_alphabet_recognizer_model()
+
+    logger.debug(f"AI Model loaded successfully")
 
     # Setup Flask app
     app = Flask(__name__)
@@ -52,7 +78,8 @@ def main():
 
     # Endpoints
     app.add_url_rule('/ping', view_func=ping, methods=['GET'])
-    app.add_url_rule('/get-alphabet', view_func=get_alpahabet, methods=['POST'])
+    app.add_url_rule('/get-alphabet', view_func=get_alpahabet, methods=['POST'],
+                     defaults={"hand_tracker": hand_tracker, "alphabet_recognizer": alphabet_recognizer})
 
 
     if args.debug:
--- src/endpoints/get_alphabet.py
+++ src/endpoints/get_alphabet.py
@@ -1,22 +1,61 @@
 import os
-import time
 
 import cv2
-from flask import Flask, request, jsonify
+from flask import request, jsonify
 
-import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
-from mediapipe.framework.formats import landmark_pb2
-from mediapipe.tasks.python.vision.hand_landmarker import *
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
+import numpy as np
+import io
+from PIL import Image, ImageOps
 
 from src.alphabet_recognizer import *
 
+import mediapipe as mp
+from mediapipe.tasks.python.vision.hand_landmarker import *
 
-def get_alpahabet():
+def rescale_from_smallest_side(image, target_size):
+    original_height, original_width = image.shape[:2]
+
+    target_width = target_size  # Specify the desired width
+    target_height = target_size  # Specify the desired height
+
+    if original_width > original_height:
+        target_height = int(original_height / original_width * target_width)
+    else:
+        target_width = int(original_width / original_height * target_height)
+
+    return cv2.resize(image, (target_width, target_height))
+
+def display_hand_tracked_image(image, recognition_result: HandLandmarkerResult):
+    from mediapipe.framework.formats import landmark_pb2
+
+    mp_hands = mp.solutions.hands
+    mp_drawing = mp.solutions.drawing_utils
+    mp_drawing_styles = mp.solutions.drawing_styles
+
+    # print(recognition_result)
+
+    current_frame = image
+    for hand_index, hand_landmarks in enumerate(recognition_result.hand_landmarks):
+
+        # Draw hand landmarks on the frame
+        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+        hand_landmarks_proto.landmark.extend([
+          landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y,
+                                          z=landmark.z) for landmark in
+          hand_landmarks
+        ])
+        mp_drawing.draw_landmarks(
+          current_frame,
+          hand_landmarks_proto,
+          mp_hands.HAND_CONNECTIONS,
+          mp_drawing_styles.get_default_hand_landmarks_style(),
+          mp_drawing_styles.get_default_hand_connections_style())
+
+    cv2.imshow('gesture_recognition', current_frame)
+    cv2.waitKeyEx(1000)
+    cv2.destroyAllWindows()
+
+def get_alpahabet(hand_tracker: HandLandmarker, alphabet_recognizer: LSFAlphabetRecognizer):
     if 'file' not in request.files:
         return jsonify({'error': 'No file part in the request'}), 400
 
@@ -26,63 +65,26 @@ def get_alpahabet():
         return jsonify({'error': 'No selected file'}), 400
 
     if file:
-        file.save(file.filename)
-        image = cv2.imread(file.filename)
-        os.remove(file.filename)
+        # Read the file into memory
+        in_memory_file = io.BytesIO(file.read())
+        pil_image = Image.open(in_memory_file)
 
-        original_height, original_width = image.shape[:2]
+        # pil_image.save("test.jpg")
 
-        target_width = 480  # Specify the desired width
-        target_height = 480  # Specify the desired height
+        # Convert PIL image to OpenCV format
+        pil_image = ImageOps.exif_transpose(pil_image)
+        image = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
 
-        if original_width > original_height:
-            target_height = int(original_height / original_width * target_width)
-        else:
-            target_width = int(original_width / original_height * target_height)
+        image = rescale_from_smallest_side(image, 480)
 
-        image = cv2.resize(image, (target_width, target_height))
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Ensure the image is in RGB format
 
-        base_options = python.BaseOptions(model_asset_path="hand_landmarker.task")
-        options: HandLandmarkerOptions = vision.HandLandmarkerOptions(base_options=base_options,
-                                                                      num_hands=1,
-                                                                      min_hand_detection_confidence=0,
-                                                                      min_hand_presence_confidence=0.1,
-                                                                      min_tracking_confidence=0)
-        recognizer: HandLandmarker = vision.HandLandmarker.create_from_options(options)
-
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_rgb)
-        recognition_result: HandLandmarkerResult = recognizer.detect(mp_image)
-        print(recognition_result)
-        # print(recognition_result)
 
-        current_frame = image
-        for hand_index, hand_landmarks in enumerate(recognition_result.hand_landmarks):
+        recognition_result: HandLandmarkerResult = hand_tracker.detect(mp_image)
 
-            # Draw hand landmarks on the frame
-            hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-            hand_landmarks_proto.landmark.extend([
-              landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y,
-                                              z=landmark.z) for landmark in
-              hand_landmarks
-            ])
-            mp_drawing.draw_landmarks(
-              current_frame,
-              hand_landmarks_proto,
-              mp_hands.HAND_CONNECTIONS,
-              mp_drawing_styles.get_default_hand_landmarks_style(),
-              mp_drawing_styles.get_default_hand_connections_style())
-
-        recognition_frame = current_frame
+        # display_hand_tracked_image(image, recognition_result)
 
         if len(recognition_result.hand_world_landmarks) < 1:
             return jsonify({'message': None}), 200
-
-        alphabet_model = LSFAlphabetRecognizer()
-        alphabet_model.load_state_dict(torch.load('model.pth'))
-
-
-        # cv2.imshow('gesture_recognition', recognition_frame)
-        # cv2.waitKeyEx(1000)
-        # cv2.destroyAllWindows()
-        return jsonify({'message': LABEL_MAP.id[alphabet_model.use(LandmarksTo1DArray(recognition_result.hand_world_landmarks[0]))]}), 200
+        return jsonify({'message': LABEL_MAP.id[alphabet_recognizer.use(LandmarksTo1DArray(recognition_result.hand_world_landmarks[0]))]}), 200
