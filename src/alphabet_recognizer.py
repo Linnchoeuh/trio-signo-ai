295606c16bfe40bbfecc6ccb4994029c1982b299
Linnchoeuh <lennintendo@gmail.com>
2024-07-03 16:48:26 +0200

    feat: Adding a 27th output neuron in the model for non sign hand position

--- src/alphabet_recognizer.py
+++ src/alphabet_recognizer.py
@@ -84,7 +84,7 @@ class LSFAlphabetRecognizer(nn.Module):
         super(LSFAlphabetRecognizer, self).__init__()
         self.fc1 = nn.Linear(63, 128)
         self.fc2 = nn.Linear(128, 64)
-        self.fc3 = nn.Linear(64, 26)
+        self.fc3 = nn.Linear(64, 27)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
