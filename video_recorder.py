2b035fcdac4fc6f617c006cde33dd438034ef24d
JeanPierre Janopoulos <jean-pierre.janopoulos@epitech.eu>
2024-10-01 20:02:19 +0200

    [feat] : every keyboard key can now take screenshots of the set label

--- video_recorder.py
+++ video_recorder.py
@@ -1,17 +1,54 @@
 import os
 import cv2
+import json
 import numpy as np
 import tkinter as tk
-from tkinter import simpledialog
 from datetime import datetime
-import json
+from tkinter import simpledialog
 from src.video_cropper import VideoCropper
 
 ESC = 27
 SPACE = 32
 A_KEY = 97
 B_KEY = 98
-fps = 30
+FPS = 30
+
+keys_index = {'a' : 'a',
+        'b' : 'b',
+        'c' : 'c',
+        'd' : 'd',
+        'e' : 'e',
+        'f' : 'f',
+        'g' : 'g',
+        'h' : 'h',
+        'i' : 'i',
+        'j' : 'j',
+        'k' : 'k',
+        'l' : 'l',
+        'm' : 'm',
+        'n' : 'n',
+        'o' : 'o',
+        'p' : 'p',
+        'q' : 'q',
+        'r' : 'r',
+        's' : 's',
+        't' : 't',
+        'u' : 'u',
+        'v' : 'v',
+        'w' : 'w',
+        'x' : 'x',
+        'y' : 'y',
+        'z' : 'z',
+        '1' : '1',
+        '2' : '2',
+        '3' : '3',
+        '4' : '4',
+        '5' : '5',
+        '6' : '6',
+        '7' : '7',
+        '8' : '8',
+        '9' : '9',
+        '0' : '0'}
 
 save_dir_videos = 'datasets/source_videos'
 save_dir_screenshots = 'datasets/source_images/screenshots'
@@ -100,7 +137,7 @@ while True:
                 if video_name:
                     file_name = video_name + "_" + current_time + ".avi"
                     output_file = os.path.join(save_dir_videos, file_name)
-                    out = cv2.VideoWriter(output_file, fourcc, fps, (frame_width, frame_height))
+                    out = cv2.VideoWriter(output_file, fourcc, FPS, (frame_width, frame_height))
                     is_recording = True
                     print("Starting to record...")
             else:
@@ -108,19 +145,9 @@ while True:
                 out.release()
                 print(f"Recording saved in {output_file}.")
                 update_json(videos_json_path, {"filename": output_file, "label": video_name})
-
-        # Take a screenshot, ask for the label and save a trace in a json
-        elif key == A_KEY:
-            image_name = file_name_popup("the screenshot")
-            if image_name:
-                file_name = image_name + "_" + current_time + ".png"
-                output_file = os.path.join(save_dir_screenshots, file_name)
-                cv2.imwrite(output_file, frame)
-                print(f"Screenshot saved in {output_file}.")
-                update_json(screenshots_json_path, {"filename": file_name, "label": image_name})
-
-        elif key == B_KEY:
-            is_croping = True
+        
+        #elif key == B_KEY:
+        #    is_croping = True
         
         # Quit
         elif key == ESC:
@@ -128,6 +155,17 @@ while True:
                 break
             else:
                 print("Stop recording before quitting.")
+
+        # Take a screenshot, ask for the label and save a trace in a json
+        for keys in keys_index.keys():
+            if (key == ord(keys)):
+                print(keys_index[keys])
+                image_name = keys_index[keys]
+                file_name = image_name + "_" + current_time + ".png"
+                output_file = os.path.join(save_dir_screenshots, file_name)
+                cv2.imwrite(output_file, frame)
+                print(f"Screenshot saved in {output_file}.")
+                update_json(screenshots_json_path, {"filename": file_name, "label": image_name})
 
     # First attempt at switching back and forth between video record and video crop
     else:
