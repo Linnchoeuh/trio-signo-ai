d27378be5b7decc3c15db3de45c195cd937c051b
Linnchoeuh <lennintendo@gmail.com>
2024-11-21 06:42:40 +0100

    feat: adding gpu acceleration

--- train_model.py
+++ train_model.py
@@ -8,6 +8,8 @@ import json
 from src.datasample import *
 from src.model_class.sign_recognizer_v1 import *
 
+import torch
+
 parser: argparse.ArgumentParser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument(
@@ -34,12 +36,31 @@ parser.add_argument(
     help='Name of the model',
     required=False,
     default=20)
+parser.add_argument(
+    '--device',
+    help='The device to use for training options: (cpu, gpu, cuda, mps)',
+    required=False,
+    default="gpu")
+
 
 args: argparse.Namespace = parser.parse_args()
 
 train_data: TrainData2 = TrainData2.from_cbor_file(args.trainset)
 
-
+device = torch.device("cpu")
+if args.device in ["gpu", "cuda"] and torch.cuda.is_available():
+    # Check for CUDA (NVIDIA GPU)
+    device = torch.device("cuda")
+    print("Using NVIDIA GPU with CUDA")
+    print("GPU Name:", torch.cuda.get_device_name(torch.cuda.current_device()), "ID:", torch.cuda.current_device())
+elif args.device in ["gpu", "mps"] and torch.backends.mps.is_available():  # On ROCm-enabled PyTorch builds
+    # Check for ROCm (AMD GPU)
+    device = torch.device("mps")
+    print("Using AMD GPU with ROCm")
+    print("GPU Name:", torch.cuda.get_device_name(torch.cuda.current_device()), "ID:", torch.cuda.current_device())
+else:
+    # Default to CPU
+    print("Using CPU")
 
 
 match args.arch:
@@ -49,7 +70,7 @@ match args.arch:
             train_data.info.active_gestures,
             train_data.info.labels,
             name=args.name,
-            intermediate_layers=[8]))
+            intermediate_layers=[8]), device=device)
         validation_data: TrainData2 = None
         train_data, validation_data = train_data.split_trainset(0.8)
         # print(train_data.getNumberOfSamples(), validation_data.getNumberOfSamples())
