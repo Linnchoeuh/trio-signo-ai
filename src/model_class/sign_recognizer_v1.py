a22641887dab9279093e1691281ceb7155bc3dc3
Linnchoeuh <lennintendo@gmail.com>
2024-11-28 18:42:58 +0900

    wip: testing a new to train the model, by automatizing the the number and the size of layer

--- src/model_class/sign_recognizer_v1.py
+++ src/model_class/sign_recognizer_v1.py
@@ -11,6 +11,7 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 
 from src.datasample import *
+from src.train_model.AccuracyCalculator import AccuracyCalculator
 
 @dataclass
 class ModelInfo(TrainDataInfo):
@@ -43,6 +44,9 @@ class ModelInfo(TrainDataInfo):
         with open(file_path, 'r', encoding="utf-8") as f:
             return cls.from_dict(json.load(f))
 
+    def set_intermediate_layers(self, layers: list[int]):
+        self.layers = [self.layers[0]] + layers + [self.layers[-1]]
+
     def to_dict(self):
         _dict: dict = super(ModelInfo, self).to_dict()
         _dict["layers"] = self.layers
@@ -63,51 +67,8 @@ class ModelEpochResult:
     mean_loss: float
     epoch: int
 
-class AccuracyCalculator:
-    def __init__(self, num_classes: int):
-        self.num_classes = num_classes
-        self.correct_per_class = None
-        self.total_per_class = None
-        self.reset()
-
-    def reset(self):
-        self.correct_per_class = [0] * self.num_classes # To track correct predictions per class
-        self.total_per_class = [0] * self.num_classes # To track total samples per class
-
-    def calculate_accuracy(self, outputs: torch.Tensor, labels):
-        # Get predictions
-        _, predictions = torch.max(outputs, 1)  # Predicted class indices
-
-        # Update correct and total counts for each class
-        for label in range(self.num_classes):
-            self.correct_per_class[label] += ((predictions == label) & (labels == label)).sum().item()
-            self.total_per_class[label] += (labels == label).sum().item()
-
-    def get_accuracy(self) -> tuple[float, dict]:
-        avg_accuracy = sum(self.correct_per_class) / sum(self.total_per_class) if sum(self.total_per_class) > 0 else 0
-        return (avg_accuracy, [correct / total if total > 0 else 0 for correct, total in zip(self.correct_per_class, self.total_per_class)])
-
-def print_accuraccy_table(accuracies: list[float], labels: list[str],
-                          color_chart: list[tuple[float, tuple[int, int, int]]] = [
-                              (0, (192, 0, 0)),
-                              (0.5, (255, 0, 0)),
-                              (0.7, (255, 50, 0)),
-                              (0.8, (255, 150, 0)),
-                              (0.9, (255, 255, 0)),
-                              (0.95, (32, 255, 0)),
-                              (0.99, (0, 200, 128)),
-                              (1, (0, 0, 255))
-                          ]):
-    for i, label in enumerate(labels):
-        for k in range(len(color_chart)):
-            r, g, b = color_chart[k][1]
-            if (k < len(color_chart) - 1 and accuracies[i] >= color_chart[k][0] and accuracies[i] <= color_chart[k + 1][0]) \
-            or (k >= len(color_chart) - 1 and accuracies[i] >= color_chart[k][0]):
-                print(f"\033[38;2;{r};{g};{b}m", end="")
-        print(f"\t{label}: {(accuracies[i] * 100):.2f}%\033[0m")
-
 class SignRecognizerV1(nn.Module):
-    def __init__(self, model_info: ModelInfo, device: torch.device = torch.device("cpu")):
+    def __init__(self, model_info: ModelInfo, device: torch.device = torch.device("cpu"), dropout: float = 0.3):
         super(SignRecognizerV1, self).__init__()
 
         self.info = model_info
@@ -117,7 +78,7 @@ class SignRecognizerV1(nn.Module):
         for i in range(len(model_info.layers) - 1):
             self.fcs.append(nn.Linear(model_info.layers[i], model_info.layers[i+1]))
             if i < len(model_info.layers) - 2:
-                self.fcs.append(nn.Dropout(0.3))
+                self.fcs.append(nn.Dropout(dropout))
 
         self.to(self.device)
 
@@ -165,120 +126,3 @@ class SignRecognizerV1(nn.Module):
             logits = self(input_tensor)
         probabilities = F.softmax(logits, dim=0)
         return torch.argmax(probabilities, dim=0).item()
-
-    def trainModel(self, train_data: TrainData2, num_epochs: int = 20, validation_data: TrainData2 = None, balance_weights: int = 1) -> str:
-        model_epochs: list[ModelEpochResult] = []
-        class CustomDataset(Dataset):
-            def __init__(self, input: list[list[float]], output: list[int], model_input_neuron: int):
-                if len(input) != len(output):
-                    raise ValueError("Input and output data must have the same length")
-
-                self.input: list[list[float]] = input
-                self.output: list[int] = output
-                self.model_input_neuron = model_input_neuron
-                # print(len(input), len(output))
-
-            def __len__(self):
-                return len(self.input)
-
-            def __getitem__(self, idx):
-                input_data: list[float] = self.input[idx]
-                # print(input_data)
-                while len(input_data) < self.model_input_neuron:
-                    input_data.append(0)
-                input_tensor = torch.tensor(input_data, dtype=torch.float32)
-                label_tensor = torch.tensor(self.output[idx], dtype=torch.long)
-                return input_tensor, label_tensor
-
-        dataset: CustomDataset = CustomDataset(train_data.get_input_data(), train_data.get_output_data(), self.info.layers[0])
-        dataloader: DataLoader = DataLoader(dataset, batch_size=64, shuffle=True)
-        if validation_data is not None:
-            validation_set: CustomDataset = CustomDataset(validation_data.get_input_data(), validation_data.get_output_data(), self.info.layers[0])
-            validation_dataloader: DataLoader = DataLoader(validation_set, batch_size=64, shuffle=True)
-
-        criterion = nn.CrossEntropyLoss(train_data.get_class_weights(balance_weights))
-        criterion.to(self.device)
-
-        optimizer = optim.Adam(self.parameters(), lr=0.001, weight_decay=1e-4)
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5)
-
-        train_accuracy_calculator = AccuracyCalculator(len(self.info.labels))
-        validation_accuracy_calculator = AccuracyCalculator(len(self.info.labels))
-
-
-        start_time = time.time()
-        for epoch in range(num_epochs):
-            train_accuracy_calculator.reset()
-            val_los: float = 0
-            for inputs, labels in dataloader:
-                inputs, labels = inputs.to(self.device), labels.to(self.device)
-                outputs = self(inputs)
-                loss = criterion(outputs, labels)
-
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-
-                train_accuracy_calculator.calculate_accuracy(outputs, labels)
-
-            val_loss = loss.item()
-
-            lr = optimizer.param_groups[0]['lr']
-
-            print(f"--- " +
-                f"Epoch [{epoch+1}/{num_epochs}], " +
-                f"Remaining time: {time.strftime('%H:%M:%S', time.gmtime(((time.time() - start_time) / (epoch+1)) * (num_epochs - epoch - 1)))}, " +
-                f"Learning Rate: {lr:.6f}" +
-                f" ---")
-            train_avg_acc, train_accuracy = train_accuracy_calculator.get_accuracy()
-            print(f"\tTrain Loss: {loss.item():.4f}, " +
-                f"Train Accuracy: {(train_avg_acc * 100):.2f}%")
-            # for i, label in enumerate(self.info.labels):
-            #     print(f"\t\t{label}: {(train_accuracy[i] * 100):.2f}%")
-            print_accuraccy_table(train_accuracy, self.info.labels)
-            if validation_data is not None:
-                total_correct = 0
-                validation_loss = None
-                validation_accuracy_calculator.reset()
-                for inputs, labels in validation_dataloader:
-                    inputs, labels = inputs.to(self.device), labels.to(self.device)
-                    outputs = self(inputs)
-                    validation_loss = criterion(outputs, labels)
-
-                    validation_accuracy_calculator.calculate_accuracy(outputs, labels)
-
-                validation_avg_acc, validation_accuracy = validation_accuracy_calculator.get_accuracy()
-                print(f"\tValidation Loss: {validation_loss.item():.4f}, " +
-                    f"Validation accuracy: {(validation_avg_acc * 100):.2f}%")
-                # for i, label in enumerate(self.info.labels):
-                #     print(f"\t\t{label}: {(validation_accuracy[i] * 100):.2f}%")
-                print_accuraccy_table(validation_accuracy, self.info.labels)
-
-                loss_diff: float = abs(loss.item() - validation_loss.item())
-                mean_loss: float = (loss.item() + validation_loss.item()) / 2
-
-                val_loss += validation_loss.item()
-
-                print(f"\tLoss Diff: {loss_diff:.4f}, Mean Loss: {mean_loss:.4f}")
-
-            scheduler.step(val_loss)
-
-            model_epochs.append(ModelEpochResult(copy.deepcopy(self), loss.item(),
-                                                 validation_loss.item() if validation_loss is not None else None,
-                                                 loss_diff, mean_loss, epoch+1))
-
-        """
-        model_epochs.sort(key=lambda x: x.loss_diff)
-        # print(model_epochs)
-        while len(model_epochs) > 5:
-            model_epochs.pop(-1)
-        # for model_epoch in model_epochs:
-        #     print(f"Epoch {model_epoch.epoch}: Loss diff: {model_epoch.loss_diff:.4f}, Mean Loss: {model_epoch.mean_loss:.4f}")
-        model_epochs.sort(key=lambda x: x.validation_loss)
-        # print("-----")
-        # for model_epoch in model_epochs:
-        #     print(f"Epoch {model_epoch.epoch}: Loss diff: {model_epoch.loss_diff:.4f}, Mean Loss: {model_epoch.mean_loss:.4f}")
-        # print(model_epochs)
-        print("Model Epoch Picked:", model_epochs[0].epoch)
-        self = model_epochs[0].model_instance
-        """
---
+++ src/train_model/train.py
@@ -0,0 +1,151 @@
+import time
+
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim as optim
+import torch
+
+from src.train_model.AccuracyCalculator import AccuracyCalculator
+from src.train_model.CustomDataset import CustomDataset
+from src.model_class.sign_recognizer_v1 import SignRecognizerV1
+
+def train_epoch_run_model(model: SignRecognizerV1, criterion: nn.Module, inputs: torch.Tensor, labels: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+    """_summary_
+
+    Args:
+        model (SignRecognizerV1): _description_
+        criterion (nn.CrossEntropyLoss): _description_
+        inputs (torch.Tensor): _description_
+        labels (torch.Tensor): _description_
+
+    Returns:
+        tuple[torch.Tensor, torch.Tensor]: tuple(loss, outputs)
+    """
+    inputs, labels = inputs.to(model.device), labels.to(model.device)
+    outputs: torch.Tensor = model(inputs)
+    return (criterion(outputs, labels), outputs)
+
+def train_epoch_optimize(optimizer: optim.Optimizer, loss: torch.Tensor):
+    """_summary_
+
+    Args:
+        optimizer (optim.Optimizer): _description_
+        loss (torch.Tensor): _description_
+    """
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+def train_epoch(model: SignRecognizerV1, dataloader: DataLoader, criterion: nn.Module, optimizer: optim.Optimizer) -> tuple[torch.Tensor, AccuracyCalculator]:
+    """Will run the model and then optimize it.
+
+    Args:
+        model (SignRecognizerV1): Model to run
+        dataloader (DataLoader): Data to run the model on
+        criterion (nn.Module): Loss function
+        optimizer (optim.Optimizer): Optimizer
+
+    Returns:
+        tuple[torch.Tensor, AccuracyCalculator]: tuple(loss, accuracy_calculator)
+    """
+    model.train()
+    accuracy_calculator: AccuracyCalculator = AccuracyCalculator(model.info.labels)
+
+    for inputs, labels in dataloader:
+        loss, outputs = train_epoch_run_model(model, criterion, inputs, labels)
+        train_epoch_optimize(optimizer, loss)
+
+        accuracy_calculator.calculate_accuracy(outputs, labels)
+
+    return loss, accuracy_calculator
+
+def validation_epoch(model: SignRecognizerV1, dataloader: DataLoader, criterion: nn.Module) -> tuple[torch.Tensor, AccuracyCalculator]:
+    """Will run the model without doing the optimization part (Use <strong>train_epoch</strong> function for that).
+
+    Args:
+        model (SignRecognizerV1): Model to run
+        dataloader (DataLoader): Data to run the model on
+        criterion (nn.Module): Loss function
+
+    Returns:
+        tuple[torch.Tensor, AccuracyCalculator]: tuple(loss, accuracy_calculator)
+    """
+    model.eval()
+    accuracy_calculator: AccuracyCalculator = AccuracyCalculator(model.info.labels)
+
+    with torch.no_grad():
+        for inputs, labels in dataloader:
+            loss, outputs = train_epoch_run_model(model, criterion, inputs, labels)
+
+            accuracy_calculator.calculate_accuracy(outputs, labels)
+
+    return loss, accuracy_calculator
+
+def log_validation_info(val_acc: AccuracyCalculator, loss: torch.Tensor, val_loss: torch.Tensor):
+    loss_diff: float = abs(loss.item() - val_loss.item())
+    mean_loss: float = (loss + val_loss.item()) / 2
+
+    validation_avg_acc, _ = val_acc.get_accuracy()
+    print(f"\tValidation Loss: {val_loss.item():.4f}, " +
+        f"Validation accuracy: {(validation_avg_acc * 100):.2f}%")
+    val_acc.print_accuracy_table()
+    print(f"\tLoss Diff: {loss_diff:.4f}, Mean Loss: {mean_loss:.4f}")
+
+def train_model(model: SignRecognizerV1, device: torch.device,
+                train_data: DataLoader, validation_data: DataLoader = None,
+                num_epochs: int = 20, weights_balance: torch.Tensor = None,
+                learning_rate: float = 0.001, validation_interval: int = 2, silent: bool = False) -> float:
+
+    model.to(device)
+
+    criterion: nn.Module = nn.CrossEntropyLoss()
+    if weights_balance is not None:
+        criterion.weight = weights_balance
+    criterion.to(device)
+    optimizer: optim.Optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    scheduler: optim.lr_scheduler.ReduceLROnPlateau = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5)
+
+    start_time = time.time()
+    remain_time: str = "Estimating..."
+    final_loss: float = 0
+    validation_was_runned: bool = False
+    for epoch in range(num_epochs):
+        validation_was_runned = False
+        loss, train_acc = train_epoch(model, train_data, criterion, optimizer)
+        final_loss = loss.item()
+
+        if not silent:
+            print(f"--- " +
+                f"Epoch [{epoch+1}/{num_epochs}], " +
+                f"Remaining time: {remain_time}, " +
+                f"Learning Rate: {(optimizer.param_groups[0]['lr']):.6f}" +
+                f" ---")
+            train_avg_acc, _ = train_acc.get_accuracy()
+            print(f"\tTrain Loss: {loss.item():.4f}, " +
+                f"Train Accuracy: {(train_avg_acc * 100):.2f}%")
+            train_acc.print_accuracy_table()
+
+
+
+        if validation_data is not None and validation_interval > 1 and epoch % validation_interval == validation_interval - 1:
+            validation_was_runned = True
+            val_loss, val_acc = validation_epoch(model, validation_data, criterion)
+            final_loss = val_loss.item()
+
+            if not silent:
+                log_validation_info(val_acc, loss, val_loss)
+
+
+
+        scheduler.step(final_loss)
+        remain_time = time.strftime('%H:%M:%S', time.gmtime(((time.time() - start_time) / (epoch+1)) * (num_epochs - epoch - 1)))
+
+    if validation_data is not None and not validation_was_runned:
+        val_loss, val_acc = validation_epoch(model, validation_data, criterion)
+        final_loss = val_loss.item()
+
+        if not silent:
+            log_validation_info(val_acc, loss, val_loss)
+
+
+    return final_loss
