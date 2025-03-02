88c50d917355769c8586863548453fc7a4e89566
Linnchoeuh <lennintendo@gmail.com>
2025-03-03 00:00:19 +0900

    feat: onnx runner script to check if model runs properly after conversion

--- src/api/endpoints/get_sign_recognizer_model.py
+++ src/api/endpoints/get_sign_recognizer_model.py
@@ -3,6 +3,6 @@ from flask import send_from_directory
 
 def get_sign_recognizer_model(model_name: str):
     # model_name = model_name.replace("/", "")
-    if not model_name.endswith(".onnx"):
-        model_name = f"{model_name}.onnx"
+    if not model_name.endswith(".zip"):
+        model_name = f"{model_name}.zip"
     return send_from_directory(directory="onnx_models", path=model_name, as_attachment=True)
